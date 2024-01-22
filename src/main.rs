d039bdb0412376329a0bdeede8c4590d943fbe27
infernostars <infernity@infernity.dev>
2024-01-22 13:56:19 -0700

    vga!! real

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,19 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
+use crate::vga_buffer::{Color, ColorCode};
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println_colored!(ColorCode::new(Color::LightRed, Color::Black), "{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("crabOS!! real");
+    println!("me wen i code");
+    panic!("help im bad at coding");
     loop {}
 }
